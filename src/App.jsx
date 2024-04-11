edca63d94b22f4cb0a4929e1af6a5359444f7c6f
Mathiasfx <mathias_fx@yahoo.com.ar>
2024-04-11 08:50:36 -0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,36 @@ import Sorteo from "./Sorteo";
 
 function App() {
   const [csvData, setCsvData] = useState([]);
+  const [fileUploaded, setFileUploaded] = useState(false); // Nuevo estado para verificar si se cargó el archivo
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     Papa.parse(file, {
       complete: (result) => {
-        const validData = result.data.filter(item => item.Nombre && item.Apellido); // Filtrar campos vacíos
+        const validData = result.data.filter((item) => item.Nombre);
         setCsvData(validData);
+        setFileUploaded(true); // Establecer que se cargó el archivo
       },
       header: true,
       delimiter: ";",
     });
   };
 
+  const confirmFileUpload = () => {
+    // Simplemente establecer el estado de fileUploaded en true
+    setFileUploaded(true);
+  };
+
   return (
     <>
-      {csvData.length > 0 ? (
+      {csvData.length > 0 || fileUploaded ? ( // Verificar si se cargó el archivo o si ya existe data
         <Sorteo nombres={csvData} setCsvData={setCsvData} />
       ) : (
-        <input type="file" accept=".csv" onChange={handleFileChange} />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <input type="file" accept=".csv" onChange={handleFileChange} />
+          {/* Botón para confirmar la carga del archivo */}
+          <button onClick={confirmFileUpload}>Confirmar carga</button>
+        </div>
       )}
     </>
   );
--- src/Sorteo.jsx
+++ src/Sorteo.jsx
@@ -3,82 +3,96 @@ import { useEffect, useState } from "react";
 import Confetti from "react-confetti";
 import useWindowSize from "react-use/lib/useWindowSize";
 
-// eslint-disable-next-line react/prop-types
 const Sorteo = ({ nombres, setCsvData }) => {
+  // Obtenemos el tamaño de la ventana para el efecto de confeti
   const { width, height } = useWindowSize();
+
+  // Estado para controlar si se está realizando el sorteo o no
   const [isRendering, setIsRendering] = useState(true);
+
+  // Estado para almacenar los nombres disponibles para el sorteo
   const [nombresDisponibles, setNombresDisponibles] = useState([...nombres]);
+
+  // Estado para almacenar los nombres seleccionados durante el sorteo
   const [nombresSeleccionados, setNombresSeleccionados] = useState([]);
+
+  // Estado para almacenar el nombre actual que se está mostrando
   const [nombre, setNombre] = useState("");
+
+  // Intervalo de tiempo para generar nombres aleatorios durante el sorteo
   const interval = 50;
 
   useEffect(() => {
+    // Efecto para generar nombres aleatorios mientras el sorteo esté activo
     if (isRendering && nombres.length > 0) {
       const i = setInterval(() => {
-        const randomIndex = Math.floor(Math.random() * nombres.length);
-        const randomNombre = nombres[randomIndex]?.Nombre || "-";
-        const randomApellido = nombres[randomIndex]?.Apellido || "-";
-        setNombre(`${randomNombre} ${randomApellido}`);
-   
+        // Generar un índice aleatorio dentro del rango de nombres disponibles
+        const randomIndex = Math.floor(
+          Math.random() * nombresDisponibles.length
+        );
+        // Obtener el nombre aleatorio correspondiente al índice generado
+        const randomNombre = nombresDisponibles[randomIndex]?.Nombre || "-";
+        // Actualizar el nombre que se está mostrando
+        setNombre(randomNombre);
       }, interval);
       return () => clearInterval(i);
     }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isRendering, nombres, interval]);
+    // Imprimir en la consola los nombres disponibles (solo para propósitos de depuración)
+    console.log(nombresDisponibles);
+  }, [isRendering, nombres, nombresDisponibles, interval]);
 
   const seleccionarNombres = () => {
+    // Cambiar el estado de isRendering para iniciar o detener el sorteo
     setIsRendering(!isRendering);
+
+    // Si el sorteo está iniciando
     if (isRendering) {
-      const nombresAleatorios = [];
-      const nombresRestantes = nombresDisponibles.filter((_, index) => !nombresSeleccionados.includes(index));
+      // Generar un nombre aleatorio una vez
+      const randomIndex = Math.floor(Math.random() * nombresDisponibles.length);
+      const nombreAleatorio = nombresDisponibles[randomIndex];
 
-      while (nombresAleatorios.length < 20 && nombresRestantes.length > 0) {
-        const randomIndex = Math.floor(Math.random() * nombresRestantes.length);
-        nombresAleatorios.push(nombresRestantes[randomIndex]);
-        nombresRestantes.splice(randomIndex, 1);
-      }
+      // Agregar el nombre aleatorio seleccionado a la lista de nombres seleccionados
+      setNombresSeleccionados([...nombresSeleccionados, nombreAleatorio]);
 
-      setNombresSeleccionados(nombresSeleccionados.concat(nombresAleatorios));
-
-      const updatedNombresDisponibles = nombresDisponibles.filter((_, index) => !nombresSeleccionados.includes(index));
-    
-      setCsvData(updatedNombresDisponibles);
+      // Actualizar la lista de nombres disponibles eliminando el nombre seleccionado
+      const updatedNombresDisponibles = nombresDisponibles.filter(
+        (nombre, index) => index !== randomIndex
+      );
       setNombresDisponibles(updatedNombresDisponibles);
-  
 
-    
-     
+      // Actualizar los datos CSV si es necesario
+      setCsvData(updatedNombresDisponibles);
     }
   };
 
-  // useEffect(() => {
-  //   if (isRendering) {
-  //     const i = setInterval(seleccionarNombres, interval);
-  //     return () => clearInterval(i);
-  //   }
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [isRendering, nombresSeleccionados, interval]);
-
   return (
     <div>
-    <Confetti
-      style={!isRendering ? { display: "block" } : { display: "none" }}
-      width={width}
-      height={height}
-    />
-    <div className="contenedorNombre">
-      <h2 className="names">{isRendering ? nombre: nombresSeleccionados.map(persona => (
-              <div key={`${persona.Nombre}-${persona.Apellido}`}>
-                {`${persona.Nombre} ${persona.Apellido}`}
-                <br />
-              </div>
-            ))}</h2>
+      {/* Confeti que se muestra cuando se detiene el sorteo */}
+      <Confetti
+        style={!isRendering ? { display: "block" } : { display: "none" }}
+        width={width}
+        height={height}
+      />
+
+      {/* Mostrar el nombre actual o los nombres seleccionados */}
+      <div className="contenedorNombre">
+        <h2 className="names">
+          {isRendering
+            ? nombre // Mostrar un nombre aleatorio mientras se está realizando el sorteo
+            : nombresSeleccionados.map((persona) => (
+                <div key={`${persona.Nombre}`}>
+                  {`${persona.Nombre}`} <br />
+                </div>
+              ))}
+        </h2>
+      </div>
+
+      {/* Botón para iniciar o detener el sorteo */}
+      <button className="btnsortear" onClick={seleccionarNombres}>
+        {!isRendering ? "Sortear" : "Detener"}
+      </button>
     </div>
-    <button className="btnsortear" onClick={seleccionarNombres}>
-      {!isRendering ? "Sortear" : "Detener"}
-    </button>
-  </div>
   );
 };
 
